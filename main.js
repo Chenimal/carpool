87a7a151f985bb97cefda78e7e0f2966b8cc8b3b
孙晨 <sunchen@jrq.com>
2017-03-21 23:34:00 +0800

    * clean markers for second run

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ var map,
     'ne_lng': 114.45359691269528,
     'sw_lat': 22.11311632940318,
     'sw_lng': 113.89192088730465,
-  };
+  },
+  markers = [];
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -16,12 +17,16 @@ function initMap() {
   });
 
   $('.create_orders').on('click', function() {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
     // number of orders is also random
-    var num_orders = Math.ceil(Math.random() * 5);
+    var num_orders = 1; //Math.ceil(Math.random() * 5);
 
     for (var i = 0; i < num_orders; i++) {
       var pickup_lat_lng = getRandomCoordinate(map_bounds),
         dropoff_lat_lng = getRandomCoordinate(map_bounds);
+
       $.ajax({
         url: 'http://carpool.lalamove.com/orders/create',
         dataType: 'jsonp',
@@ -32,17 +37,17 @@ function initMap() {
         },
         success: function(res) {
           console.log(res);
-          new google.maps.Marker({
+          markers.push(new google.maps.Marker({
             position: pickup_lat_lng,
             map: map
-          });
-          new google.maps.Marker({
+          }));
+          markers.push(new google.maps.Marker({
             position: dropoff_lat_lng,
             map: map
-          });
+          }));
         },
         error: function(e) {
-          console.log('e');
+          console.log(e);
         }
       });
     }
