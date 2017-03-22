c191fa7091f7f3b46a7671ee35b6ace81b630bc8
孙晨 <sunchen@jrq.com>
2017-03-22 23:32:02 +0800

    * stack overflow

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ var map,
     lat: 22.310816,
     lng: 114.1727589
   },
+  order_colors = ['blue', 'pink', 'green', 'orange', 'purple'],
+  cur_color = 0,
   map_center_lat_lng,
   markers = [];
 
@@ -29,43 +31,50 @@ function initMap() {
       markers[i].setMap(null);
     }
     // number of orders is also random
-    var num_orders = 1; //Math.ceil(Math.random() * 5);
+    var num_orders = Math.ceil(Math.random() * 5);
 
     for (var i = 0; i < num_orders; i++) {
-      $.when(getRandomCoordinate($.Deferred()), getRandomCoordinate($.Deferred()))
-        .then(function(pickup_lat_lng, dropoff_lat_lng) {
-          $.ajax({
-            url: 'http://carpool.lalamove.com/orders/create',
-            dataType: 'jsonp',
-            jsonp: 'jsonp',
-            data: {
-              pickup_lat_lng: [pickup_lat_lng.lat(), pickup_lat_lng.lng()],
-              dropoff_lat_lng: [dropoff_lat_lng.lat(), dropoff_lat_lng.lng()]
-            },
-            success: function(res) {
-              //console.log(res);
-              var start = new google.maps.Marker({
-                position: pickup_lat_lng,
-                map: map,
-                icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-              });
-              var end = new google.maps.Marker({
-                position: dropoff_lat_lng,
-                map: map,
-                icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
-              });
-              markers.push(start, end);
-              calculateRoute(pickup_lat_lng, dropoff_lat_lng).done(function(response) {
-                directions_display.setDirections(response);
-              });
-            },
-            error: function(e) {
-              console.log(e);
-            }
-          });
-        })
+      createOrder();
     }
   });
+}
+
+/**
+ * create random order at client side
+ */
+function createOrder() {
+  $.when(getRandomAccessibleCoordinate($.Deferred()), getRandomAccessibleCoordinate($.Deferred()))
+    .then(function(pickup_lat_lng, dropoff_lat_lng) {
+      $.ajax({
+        url: 'http://carpool.lalamove.com/orders/create',
+        dataType: 'jsonp',
+        jsonp: 'jsonp',
+        data: {
+          pickup_lat_lng: [pickup_lat_lng.lat(), pickup_lat_lng.lng()],
+          dropoff_lat_lng: [dropoff_lat_lng.lat(), dropoff_lat_lng.lng()]
+        },
+        success: function(res) {
+          //console.log(res);
+          var start = new google.maps.Marker({
+            position: pickup_lat_lng,
+            map: map,
+            icon: 'http://maps.google.com/mapfiles/ms/micons/' + order_colors[cur_color % 5] + '.png'
+          });
+          var end = new google.maps.Marker({
+            position: dropoff_lat_lng,
+            map: map,
+            icon: 'http://maps.google.com/mapfiles/ms/micons/' + order_colors[(cur_color++) % 5] + '-dot.png'
+          });
+          markers.push(start, end);
+          /*calculateRoute(pickup_lat_lng, dropoff_lat_lng).done(function(response) {
+            directions_display.setDirections(response);
+          });*/
+        },
+        error: function(e) {
+          console.log(e);
+        }
+      });
+    });
 }
 
 /**
@@ -88,17 +97,19 @@ function calculateRoute(start, end) {
 }
 
 /**
- * function to create random marker(inside bounds)
+ * function to create random accessible marker(inside bounds)
  */
-function getRandomCoordinate(promise) {
+function getRandomAccessibleCoordinate(promise) {
   // random place
   var random_lat_lng = new google.maps.LatLng(map_bounds.sw_lat + (Math.random() * (map_bounds.ne_lat - map_bounds.sw_lat)),
     map_bounds.sw_lng + (Math.random() * (map_bounds.ne_lng - map_bounds.sw_lng)));
-  // assume all locations are accessible
+
   calculateRoute(map_center_lat_lng, random_lat_lng).done(function(response, status) {
     promise.resolve(random_lat_lng);
-  }).fail(function($status) {
-    getRandomCoordinate(promise);
+    // if it is not accessible, keep randomnize
+  }).fail(function(status) {
+    console.log('not accessible');
+    getRandomAccessibleCoordinate(promise);
   });
   return promise.promise();
 }
